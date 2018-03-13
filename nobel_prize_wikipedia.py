349ec446b86b8955ca940d9a8e66be4846482a97
DaniyarBakir <daniyar.bakir@nu.edu.kz>
2018-03-13 15:49:35 +0600

    finished scrapping the wikipedia page of nobel prize winners
    Changes to be committed:
    	modified:   nobel_prize_wikipedia.py

--- nobel_prize_wikipedia.py
+++ nobel_prize_wikipedia.py
@@ -3,6 +3,7 @@ import numpy as np
 from bs4 import BeautifulSoup as bs
 import urllib.request
 from pprint import pprint as pp
+import re
 
 
 # all functions are applicable to a specified link only, do not attempt to use these functions to your own custom web-page
@@ -56,14 +57,41 @@ def get_aff_table(a):
     for i, x in enumerate(a):
         # print('i, ', i, ' x ', x)
         aff_name = extract_aff_name(x)
-        html_table = x.next.next.next.next.next.next
 
-        # print(aff_name)
-        # print(aff_name)
-        # print(html_table)
+        # version1 that worked for several tables
+        # html_table = x.next.next.next.next.next.next
 
-        # aaa.append({aff_name: html_table})
-        aaa.append({'name': aff_name,
+        tmp = x
+
+        counter = 0
+        while(counter < 3):
+
+            # print('counter is ', counter)
+            html_table = tmp.find_next('tr')
+            
+
+            '''
+            # version2 which worked for all tables except the last
+            if html_table.has_attr('valign'):
+                print('breaking out')
+                break
+            else:
+                print('getting deeper')
+                tmp = html_table
+                counter = counter + 1
+            '''
+
+            # version3 which works for all tables
+            if len(html_table.select('td')) > 2:
+                # print('breaking out')
+                break
+            else:
+                # print('getting deeper')
+                tmp = html_table
+                counter = counter + 1
+
+        aaa.append({'title_aff_name': aff_name['title_aff_name'],
+                    'text_aff_name': aff_name['text_aff_name'],
                     'html_table': html_table})
 
     return aaa
@@ -71,10 +99,68 @@ def get_aff_table(a):
 
 def extract_aff_name(x):
 
-    aff_name = [x.select('a')[0].get_attribute_list('title')[0], 
-                x.select('a')[0].get_text()]
+    aff_name = {'title_aff_name': x.select('a')[0].get_attribute_list('title')[0], 
+                'text_aff_name': x.select('a')[0].get_text()}
 
     return aff_name
+
+
+def extract_data_from_col(a, aff_name):
+
+    col = a.select('td')[1]
+    lis = col.select('li')
+    re_year = re.compile(r'\b\d{4}\b')
+    re_phd = re.compile(r'\b(phd)\b', re.I)
+
+    ress = []
+
+
+    for i, x in enumerate(lis):
+
+        # if PhD --> extract
+        
+        # print('i ', '\tx ', x)
+
+        # if '(PhD)' in x.text:
+        # print(x.text)
+        # print(re_phd.match(x.text))
+        # if re_phd.match(x.text):
+        if 'phd' in x.text.lower():
+            
+            try:
+
+                field = x.find('abbr').get_attribute_list('title')[0].lower()
+                
+                tmp = x.find_next('a')
+                title_name = tmp.get_attribute_list('title')[0]
+                name = tmp.text
+                
+                # print('title_name is ', title_name, '\tname is ', name)
+                
+                year = re_year.findall(x.text)[0]
+                # print(year)
+                
+                res = {'title_aff_name': aff_name['title_aff_name'],
+                       'text_aff_name': aff_name['text_aff_name'],
+                       'year': year,
+                       'field': field,
+                       'name': name,
+                       'title_name': title_name,
+                       'degree': 'phd'}
+                
+                ress.append(res)
+
+            except:
+                pass
+
+        
+    if len(ress) == 0:
+
+        res = {'title_aff_name': aff_name['title_aff_name'],
+               'text_aff_name': aff_name['text_aff_name']}
+        return [res]
+    else:
+        return ress
 
 
 
@@ -103,39 +189,29 @@ if __name__ == "__main__":
 
     soup = bs(page, 'html.parser')
 
-
     a = soup.select('table.wikitable')
 
     tables = [x for x in a if isNeededTable(x) is True]
-    
+
     affs_in_table = []
     [affs_in_table.extend(find_aff(x.select('td'))) for x in tables]
     affs_in_dict = get_aff_table(affs_in_table)
     
-    aaa = affs_in_table[0]
+    affs_data = []
+
+    # for i in range(len(affs_in_dict[:5])):
+    for i in range(len(affs_in_dict)):
+
+        res = extract_data_from_col(affs_in_dict[i]['html_table'], 
+                                    {'title_aff_name': affs_in_dict[i]['title_aff_name'],
+                                     'text_aff_name': affs_in_dict[i]['text_aff_name']
+                                     })
+        affs_data.append(res)
+
+    # putting everything into a single df
+    df = pd.concat([pd.DataFrame(x) for x in affs_data], ignore_index=True).replace(np.nan, '')
+
+    # removing empty entries
+    df = df[df['field'] != '']
     
-    aaa2 = aaa.find_next('tr').find_next('tr')
-    counter = 0
-    while(counter < 5):
-        aaa2 = aaa.find_next('tr')
-        
-        if aaa2.has_attr('valign'):
-            break
-        else:
-            aaa = aaa2
-            counter = counter + 1
-    
-    bbb = affs_in_table[-1]
-    # bbb2 = affs_in_table[]
-
-    # a = tables[-1]
-    # affs_in_table = find_aff(a.select('td'))
-    # qqq = get_aff_table(affs_in_table)
-
-    # html_tables = affs_in_table[0].next.next.next.next.next.next
-    # html_tables = [x.next.next.next.next.next.next for x in affs_in_table]
-
-    # aaa = html_tables[0]
-
-
-    # qqq = extract_aff_name(aaa)
+    df.to_excel('wikipedia_nobel_prize.xlsx')
